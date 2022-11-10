94162dd701973fa0750025be10b37263c1949b2d
Sabry Chasseray <schasseray@emi.u-bordeaux.fr>
2022-11-10 16:01:44 +0100

    walkable box

--- src/main/java/fr/ubx/poo/ubomb/go/decor/Box.java
+++ src/main/java/fr/ubx/poo/ubomb/go/decor/Box.java
@@ -4,9 +4,16 @@
 
 package fr.ubx.poo.ubomb.go.decor;
 import fr.ubx.poo.ubomb.game.Position;
+import fr.ubx.poo.ubomb.go.Walkable;
+import fr.ubx.poo.ubomb.go.character.Player;
 
-public class Box extends Decor {
+public class Box extends Decor implements Walkable {
     public Box(Position position) {
         super(position);
+    }
+
+    @Override
+    public boolean walkableBy(Player player) {
+        return false;
     }
 }
